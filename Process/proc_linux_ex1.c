ac5abf4937af9569e559b28f306bb65115641eca
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2024-08-21 00:48:14 +0000

    feat: adicionando condicao de parada para testar o htop

--- Process/proc_linux_ex1.c
+++ Process/proc_linux_ex1.c
@@ -7,11 +7,12 @@ int value = 5;
 
 int main(){
 	pid_t pid;
-
+    char aux;
 	pid = fork();
 	
 	if (pid == 0) { /* child process */
 		value += 15;
+		aux = getchar();
 		return 0;
 	}
 	else if (pid > 0) { /* parent process */
