87a049dc3c4be3917b8c255b61fbacb406946150
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2024-08-28 00:34:13 +0000

    feat: resolvendo o exercicio 1 de threads
    O problema era rodar as 3 funcoes criadas com threads e executar corretamente em ordem

---
+++ Pthreads/exer1.c
@@ -0,0 +1,45 @@
+#include <pthread.h>
+#include <stdio.h>
+
+#include <stdlib.h>
+
+// gcc -o exer1_out exer1.c -l pthread
+
+int input;
+int sum = 5;
+
+void *readInput(){
+    printf("Digite um inteiro: ");
+    scanf("%d", &input);
+    pthread_exit(0);
+}
+
+void *sumConst(){
+    sum += input;
+    pthread_exit(0);
+}
+
+void *printResult(){
+    printf("Resultado: %d\n", sum);
+    pthread_exit(0);
+}
+
+int main(int argc, char *argv[]){
+    pthread_t tid, tid2, tid3;
+
+    pthread_attr_t attr;
+
+    pthread_attr_init(&attr);
+
+    pthread_create(&tid, &attr, readInput, NULL);
+
+    pthread_join(tid, NULL);
+
+    pthread_create(&tid2, &attr, sumConst, NULL);
+
+    pthread_join(tid2, NULL);
+
+    pthread_create(&tid3, &attr, printResult, NULL);
+
+    pthread_join(tid3, NULL);
+}
