90f341b6e995546c10473d173c29393a9a092d5f
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2024-09-17 04:57:27 +0000

    feat: scripts para teste de multiplas conexoes

--- M1/trabalhoM1/test_clients.sh
+++ M1/trabalhoM1/test_clients.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-NUM_CLIENTS=50
-WORD="palavra_teste"  # Defina a palavra que você quer enviar
+NUM_CLIENTS=500
+WORD="word_to_upper_case"  # Defina a palavra que você quer enviar
 
-# Medir o tempo de execução das conexões de strings
-echo "Medindo tempo para conexões de strings..."
+# Medir o tempo de execução das conexões
 time {
   for ((i = 1; i <= NUM_CLIENTS; i++))
   do
-    echo "$WORD" | ./string_consumer & 
+    echo "$WORD" | ./client_string &
+    echo $i | ./client_number &
   done
   wait  # Espera que todos os clientes finalizem
-
 }
-# Medir o tempo de execução das conexões de números
-# echo "Medindo tempo para conexões de números..."
-#   for ((i = 1; i <= NUM_CLIENTS; i++))
-#   do
-#     echo $i | ./number_consumer & 
-#   done
-#   wait  # Espera que todos os clientes finalizem
-# }
--- M1/trabalhoM1/test_multiple_connections.sh
+++ M1/trabalhoM1/test_multiple_connections.sh
@@ -3,19 +3,27 @@
 # Início da medição de tempo com precisão
 start=$(date +%s%N)
 
-# Testa múltiplas conexões para o socket de string
-for i in {1..50}; do
-    (
-        echo "Conexão de string $i" | nc -U -q 1 /tmp/pipestring
-    ) &
-done
+# Função para conexões de string
+test_string_connections() {
+    for i in {1..50}; do
+        (
+            echo "Conexão de string $i" | nc -U -q 1 /tmp/pipestring
+        ) &
+    done
+}
 
-# Testa múltiplas conexões para o socket de números
-# for i in {1..50}; do
-#     (
-#         echo "$i" | nc -U -q 1 /tmp/pipenumber
-#     ) &
-# done
+# Função para conexões de número
+test_number_connections() {
+    for i in {1..50}; do
+        (
+            echo "$i" | nc -U -q 1 /tmp/pipenumber
+        ) &
+    done
+}
+
+# Executa as conexões de string e número ao mesmo tempo
+test_string_connections &
+test_number_connections &
 
 # Aguarda o término de todas as conexões em segundo plano
 wait
@@ -26,5 +34,5 @@ end=$(date +%s%N)
 # Calcula a diferença de tempo em segundos com precisão (nanosegundos para segundos)
 runtime=$(echo "scale=3; ($end - $start) / 1000000000" | bc)
 
-# Exibe o tempo de execução com precisão em segundos
-echo "Tempo total de execução: $runtime segundos"
+# Exibe o tempo de execução com precisão em segundos, incluindo 0 antes do ponto decimal
+printf "Tempo total de execução: %.3f segundos\n" $runtime
