3ce16296d56cdd6fdb655aabaa2203212f2eb5cf
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2024-09-17 04:55:11 +0000

    feat: adicionando client de number e string  em codigo para testes

--- M1/trabalhoM1/client_number.c
+++ M1/trabalhoM1/client_number.c
@@ -5,61 +5,65 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
+#include <string.h>
 
 #define SOCK_PATH "/tmp/pipenumber"
+
+// gcc -o client_number client_number.c
 
 int main()
 {
     int sockfd, len;
     struct sockaddr_un remote;
-    char buffer[1024];
+    char buffer[8096];
 
-    // Create socket
+    // Cria o socket
     sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (sockfd < 0)
     {
-        perror("Falha em criar o socket");
+        perror("Erro: Falha ao criar o socket");
         return 1;
     }
 
-    // Connect to server
+    // Conecta ao servidor
     memset(&remote, 0, sizeof(remote));
     remote.sun_family = AF_UNIX;
     strncpy(remote.sun_path, SOCK_PATH, sizeof(remote.sun_path) - 1);
     len = strlen(remote.sun_path) + sizeof(remote.sun_family);
     if (connect(sockfd, (struct sockaddr *)&remote, len) < 0)
     {
-        perror("Falha em conectar no servidor");
+        perror("Erro: Falha ao conectar ao servidor");
         close(sockfd);
         return 1;
     }
 
-    // printf("Number conectado ao servidor!\n");
-
-    printf("Entre com o dado a ser enviado: ");
+    printf("Conectado ao servidor com sucesso!\n");
+    
+    // Solicita o dado ao usuário
+    printf("Digite o valor que deseja enviar ao servidor: ");
     fgets(buffer, sizeof(buffer), stdin);
 
-    // Send data to server
-    if (write(sockfd, buffer, strlen(buffer) + 1) < 0)
+    // Envia os dados ao servidor
+    if (write(sockfd, buffer, strlen(buffer) + 1) < 0) // Inclui o caractere null no envio
     {
-        perror("Falha em escrever no socket");
+        perror("Erro: Falha ao enviar os dados ao servidor");
         close(sockfd);
         return 1;
     }
 
-    // printf("Dado number enviado ao servidor.\n");
+    printf("Dados enviados ao servidor com sucesso.\n");
 
-    // Read data from server
+    // Recebe resposta do servidor
     if (read(sockfd, buffer, sizeof(buffer)) < 0)
     {
-        perror("Falha em ler do socket");
+        perror("Erro: Falha ao receber dados do servidor");
         close(sockfd);
         return 1;
     }
 
-    printf("Dado recebido: %s\n", buffer);
+    printf("Resposta do servidor: %s\n", buffer);
 
-    // Close socket and exit
+    // Fecha o socket e encerra
     close(sockfd);
     return 0;
 }
